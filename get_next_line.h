7587dabd1491a05a2b74cc5ea6e4ad7901c54b7a
YuriASN <yuriasn@hotmail.com>
2022-11-09 18:28:57 +0000

    Get next line done with bonus.

---
+++ get_next_line.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ysantos- <ysantos-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/12 16:19:04 by ysantos-          #+#    #+#             */
+/*   Updated: 2022/06/05 18:33:30 by ysantos-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stdlib.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdio.h>
+
+char	*get_next_line(int fd);
+int		has_newline(char *buffer);
+char	*bufferjoin(char *str, char *buffer);
+
+#endif
